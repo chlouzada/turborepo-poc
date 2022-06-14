c6e281da16f9106254eb58fd570b17b127e6fe48
Carlos  Fonseca <chlouzada@gmail.com>
2022-06-13 21:00:34 -0300

    fix unregistered schema

--- packages/user/src/db/index.ts
+++ packages/user/src/db/index.ts
@@ -1,3 +1,20 @@
-import { createConnection } from "mongoose";
+import { Connection, createConnection } from "mongoose";
+import { cellphoneSchema } from "../schemas/cellphone.schema";
+import { userSchema } from "../schemas/user.schema";
 
-export const db = createConnection("mongodb://localhost:27017/turborepo-user");
+let conn: Connection | undefined;
+
+export const getConnection = () => {
+  if (conn) return conn;
+
+  // Connect to MongoDB
+  conn = createConnection("mongodb://localhost:27017/turborepo-user");
+
+  // Register schemas
+  conn.model("users", userSchema);
+  conn.model("cellphones", cellphoneSchema);
+
+  return conn;
+};
+
+export const db = getConnection();
